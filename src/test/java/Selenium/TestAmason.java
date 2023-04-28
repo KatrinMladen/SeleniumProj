f42d43c7ef4fe73e49fb02a6d3ea36bbf44ee841
Katerina <mukhinakatya94@gmail.com>
2023-04-28 23:14:15 +0300

    initial commit

--- src/test/java/Selenium/TestAmason.java
+++ src/test/java/Selenium/TestAmason.java
@@ -1,5 +1,6 @@
 package Selenium;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,6 +14,7 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+
 public class TestAmason {
 
     protected WebDriver driver;
@@ -20,7 +22,11 @@ public class TestAmason {
     @BeforeTest
     public void setup() throws IOException{
         System.out.println("Before");
+
         System.setProperty("webdriver.chrome.driver", "D:\\study\\chromedriver_win32\\chromedriver.exe");
+
+        WebDriverManager.chromedriver().setup();
+        //System.setProperty("webdriver.chrome.driver", "D:\\study\\chromedriver_win32\\chromedriver.exe");
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
@@ -31,23 +37,25 @@ public class TestAmason {
     }
     @Test
     public void testSearch(){
-        try {
+
             driver.get("https://www.amazon.com/");
             WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
             searchBox.getText();
             searchBox.sendKeys("Shoes");
             searchBox.click();
-            searchBox.getRect();
+            //searchBox.getRect();
 
             WebElement searchElem = driver.findElement(By.xpath("//input[@id='nav-search-submit-button']"));
             searchElem.click();
 
             WebElement womenShoes = driver.findElement(By.xpath("//*[@id=\"n/679377011\"]/span/a"));
             womenShoes.click();
-        }
-        catch (Exception e){
-            System.out.println("Women's Walking Shoes option in not found");
-        }
+            WebElement onList = driver.findElement(By.xpath("//*[@id=\"n/679377011\"]/span/span[contains(@class,'a-size-base a-color-base a-text-bold')]"));
+            Assert.assertTrue(onList.getAttribute("class").contains("a-text-bold"),"Failed to select Women's Walking Shoes option");
+
+   //     catch (Exception e){
+  //          System.out.println("Women's Walking Shoes option in not found");
+  //      }
 
         //span[contains(text(),"Women's Walking Shoes")]
         //input[@id='679377011']
